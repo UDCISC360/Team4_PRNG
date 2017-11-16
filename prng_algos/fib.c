ba816f4ba28f333a4407a7723af8d1879ae17973
Michael Gonzalez <mikegonz@udel.edu>
2017-11-16 17:56:42 -0500

    fixed fib

--- prng_algos/fib.c
+++ prng_algos/fib.c
@@ -3,15 +3,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdint.h>
 
 #define seed 0
 #define j 5
 #define k 17
-unsigned int m = 0x7FFFFFFF;
-unsigned int array[k];
+uint32_t m = 0x7FFFFFFF;
+uint32_t array[k];
 
-unsigned int fib(){
-  unsigned int result = (array[k - j] + array[0]) & m;
+uint32_t fib(){
+  uint32_t result = (array[k - j] + array[0]) & m;
   for(int i = 1; i < k; i++) array[i-1] = array[i];
   array[k-1] = result;
   return result;
@@ -19,11 +20,11 @@ unsigned int fib(){
 int main(){
   srand(seed);
   for(int i = 0; i < k; i++) array[i] = rand() & m;
-  unsigned int num;
+  uint32_t num;
   while(1){
     printf("%u\n", num);
     num = fib();
-    sleep(1);
+    //sleep(1);
   }
   return 0;
 }
