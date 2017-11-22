7ae6799efd569964429b64397c97951d959f2299
unknown <mustafathadathil@yahoo.com>
2017-11-22 16:26:05 -0500

    Modify twister.c to work with driver.

--- libourprng.h
+++ libourprng.h
@@ -10,6 +10,8 @@ uint32_t lfg(uint32_t* lag1, uint32_t* lag2, uint32_t* modval,
 uint32_t lcg(uint32_t* state, uint32_t* modval, uint32_t* multfactor,
              uint32_t* addfactor);
 			 
+uint32_t twister(uint32_t* seed);
+			 
 uint32_t icg(uint32_t* state, uint32_t* modval, uint32_t* multfactor,
              uint32_t* addfactor);
 #endif
